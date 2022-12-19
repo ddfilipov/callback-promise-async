662af069f4902b5fe09ac2dfd201a1c93244bfe8
ddfilipov <denisfilipov12+3@gmail.com>
2022-12-19 19:27:00 +0100

    added the first directory for the callback explanation

---
+++ 1-callbacks/app.js
@@ -0,0 +1,27 @@
+function getCats(cb) {
+    const random = Math.random() * 1000;
+    setTimeout(function () {
+        cb(["Meowsalot", "Purrsloud", "BiscuitMaker"]);
+    }, random);
+}
+
+function getDogs(cb) {
+    const random = Math.random() * 1000;
+    setTimeout(function () {
+        cb(["EatsAnything", "Barksalot", "HeadTilt"]);
+    }, random);
+}
+
+function getBirds(cb) {
+    const random = Math.random() * 1000;
+    setTimeout(function () {
+        cb(["Scruffy", "Baldy", "Screech"]);
+    }, random);
+}
+
+function superSecretOrder(items, cb) {
+    const random = Math.random() * 1000;
+    setTimeout(function () {
+        cb([...items].sort());
+    }, random);
+}
