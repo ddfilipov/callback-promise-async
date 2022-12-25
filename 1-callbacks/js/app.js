d6c1e1170dd81263357a8d9e08fee1e91f28f162
ddfilipov <denisfilipov12+3@gmail.com>
2022-12-25 20:40:29 +0100

    method 2 finished

--- 1-callbacks/js/app.js
+++ 1-callbacks/js/app.js
@@ -1,21 +1,47 @@
+// METHOD 1: Execute functions 1 by 1
+// getCats(function (cats) {
+//     getDogs(function (dogs) {
+//         getBirds(function (birds) {
+//             const allAnimals = cats.concat(dogs, birds);
+//             superSecretOrder(allAnimals, function (sortedNames) {
+//                 console.log(sortedNames);
+//             });
+//             console.log(allAnimals);
+//         });
+//     });
+// });
+
+// METHOD 2: Execute all functions at the same time
+var allAnimals = [];
+
+function isArrayFilled() {
+    if (allAnimals.length === 3) {
+        const arrayAnimals = allAnimals[0].concat(allAnimals[1], allAnimals[2]);
+        superSecretOrder(arrayAnimals, function (orderedAnimals) {
+            console.log(orderedAnimals);
+        });
+    }
+}
+
+getCats(function (cats) {
+    allAnimals.push(cats);
+    isArrayFilled();
+});
+getDogs(function (dogs) {
+    allAnimals.push(dogs);
+    isArrayFilled();
+});
+getBirds(function (birds) {
+    allAnimals.push(birds);
+    isArrayFilled();
+});
+
 function getCats(cb) {
     const random = Math.random() * 1000;
     setTimeout(function () {
         cb(["Meowsalot", "Purrsloud", "BiscuitMaker"]);
     }, random);
 }
-
-getCats(function (cats) {
-    getDogs(function (dogs) {
-        getBirds(function (birds) {
-            const allAnimals = cats.concat(dogs, birds);
-            superSecretOrder(allAnimals, function (sortedNames) {
-                console.log(sortedNames);
-            });
-            console.log(allAnimals);
-        });
-    });
-});
 
 function getDogs(cb) {
     const random = Math.random() * 1000;
