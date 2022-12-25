1d6f29343f3a68582d75de072bbc8d13eedafcac
ddfilipov <denisfilipov12+3@gmail.com>
2022-12-25 19:57:50 +0100

    printing the superSecretOrder result

--- 1-callbacks/js/app.js
+++ 1-callbacks/js/app.js
@@ -9,9 +9,10 @@ getCats(function (cats) {
     getDogs(function (dogs) {
         getBirds(function (birds) {
             const allAnimals = cats.concat(dogs, birds);
-            const allAnimals2 = [...cats, ...dogs, ...birds];
+            superSecretOrder(allAnimals, function (sortedNames) {
+                console.log(sortedNames);
+            });
             console.log(allAnimals);
-            console.log(allAnimals2);
         });
     });
 });
--- notes/notes.ts
+++ notes/notes.ts
@@ -1,10 +1,17 @@
 // random notes I'm taking:
 const promesa = new Promise<string>((resolve, reject) => {
     setTimeout(() => {
-        resolve("holis");
+        resolve("respuesta promesa 1");
+    }, 1000);
+});
+
+const promesa2 = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+        resolve("respuesta promesa 2");
     }, 1000);
 });
 
 promesa.then((res) => {
     console.log(res);
+    promesa2.then((res) => console.log(res));
 });
