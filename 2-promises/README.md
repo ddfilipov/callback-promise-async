e420ce25d32ffdf04b693c8ba6ca136897f11f8c
ddfilipov <denisfilipov12+3@gmail.com>
2022-12-26 20:01:52 +0100

    more data added

--- 2-promises/README.md
+++ 2-promises/README.md
@@ -2,15 +2,43 @@
 
 We again have getCats, getDogs, and getBirds that return promises to retrieve data.
 
-```js
-function getCats(cb) {
-    const random = Math.random() * 1000;
-    setTimeout(function () {
-        cb(["Meowsalot", "Purrsloud", "BiscuitMaker"]);
-    }, random);
-}
+The functions which we are calling are at the bottom of the app.js and app.ts file.
 
-getCats(function (cats) {
+`getCats().then();` this line right here would call `getCats()`, and we don't know how long this is gonna take, but when know that after it's been called hence the `.then()`, we're gonna call a function we're gonna give it.
+
+This would be the result:
+
+```js
+getCats().then(function (cats) {
     console.log(cats);
 });
 ```
+
+### synchronous async calls (execute one by one and wait)
+
+Let's say we wanted to call the functions one by one. This is what we'd do:
+
+```js
+getCats()
+    .then(function (cats) {
+        console.log(cats);
+        return getDogs();
+    })
+    .then(function (dogs) {
+        console.log(dogs);
+        return getBirds();
+    })
+    .then(function (birds) {
+        console.log(birds);
+    });
+```
+
+So, compared to callbacks, where you'd have to keep nesting another level deep reaching a callback hell. With promises you just say on the first level of nesting as see in the code above.
+
+This is a little better than callbacks
+
+### Promise.all()
+
+The previous example is not a real scenario, in reality we don't care which one of the 3 `get` functions gets executed first.
+
+This is one of the biggest reasons we need promises. `Promise.all()` recieves an array of promises
--- 2-promises/js/app.js
+++ 2-promises/js/app.js
@@ -1,65 +1,49 @@
-// METHOD 1: Execute functions 1 by 1
-// getCats(function (cats) {
-//     getDogs(function (dogs) {
-//         getBirds(function (birds) {
-//             const allAnimals = cats.concat(dogs, birds);
-//             superSecretOrder(allAnimals, function (sortedNames) {
-//                 console.log(sortedNames);
-//             });
-//             console.log(allAnimals);
-//         });
-//     });
-// });
+getCats()
+    .then(function (cats) {
+        console.log(cats);
+        return getDogs();
+    })
+    .then(function (dogs) {
+        console.log(dogs);
+        return getBirds();
+    })
+    .then(function (birds) {
+        console.log(birds);
+    });
 
-// METHOD 2: Execute all functions at the same time
-var allAnimals = [];
-
-function isArrayFilled() {
-    if (allAnimals.length === 3) {
-        const arrayAnimals = allAnimals[0].concat(allAnimals[1], allAnimals[2]);
-        superSecretOrder(arrayAnimals, function (orderedAnimals) {
-            console.log(orderedAnimals);
-        });
-    }
+// Functions which return promises that you can use
+function getCats() {
+    return new Promise(function (resolve, reject) {
+        const random = Math.random() * 1000;
+        setTimeout(function () {
+            resolve(["Meowsalot", "Purrsloud", "BiscuitMaker"]);
+        }, random);
+    });
 }
 
-getCats(function (cats) {
-    allAnimals.push(cats);
-    isArrayFilled();
-});
-getDogs(function (dogs) {
-    allAnimals.push(dogs);
-    isArrayFilled();
-});
-getBirds(function (birds) {
-    allAnimals.push(birds);
-    isArrayFilled();
-});
-
-function getCats(cb) {
-    const random = Math.random() * 1000;
-    setTimeout(function () {
-        cb(["Meowsalot", "Purrsloud", "BiscuitMaker"]);
-    }, random);
+function getDogs() {
+    return new Promise(function (resolve, reject) {
+        const random = Math.random() * 1000;
+        setTimeout(function () {
+            resolve(["EatsAnything", "Barksalot", "HeadTilt"]);
+        }, random);
+    });
 }
 
-function getDogs(cb) {
-    const random = Math.random() * 1000;
-    setTimeout(function () {
-        cb(["EatsAnything", "Barksalot", "HeadTilt"]);
-    }, random);
+function getBirds() {
+    return new Promise(function (resolve, reject) {
+        const random = Math.random() * 1000;
+        setTimeout(function () {
+            resolve(["Scruffy", "Baldy", "Screech"]);
+        }, random);
+    });
 }
 
-function getBirds(cb) {
-    const random = Math.random() * 1000;
-    setTimeout(function () {
-        cb(["Scruffy", "Baldy", "Screech"]);
-    }, random);
-}
-
-function superSecretOrder(items, cb) {
-    const random = Math.random() * 1000;
-    setTimeout(function () {
-        cb([...items].sort());
-    }, random);
+function superSecretOrder(items) {
+    return new Promise(function (resolve, reject) {
+        const random = Math.random() * 1000;
+        setTimeout(function () {
+            resolve([...items].sort());
+        }, random);
+    });
 }
